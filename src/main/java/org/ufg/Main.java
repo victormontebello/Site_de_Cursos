ed771cdda6da41d33cf717e1f4ed532ab1f27892
victor montebello <montebellovictor@gmail.com>
2024-04-21 21:51:03 -0300

    infra config

--- src/main/java/org/ufg/Main.java
+++ src/main/java/org/ufg/Main.java
@@ -1,37 +1,16 @@
 package org.ufg;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.*;
-import org.bson.Document;
+import org.ufg.Infraestrutura.MongoConfig;
+
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String connectionString = "mongodb+srv://grupoufgcloud:ufgcloud001@cluster0.tch5utf.mongodb.net/?retryWrites=true&w=majority";
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
-            databases.forEach(db -> System.out.println(db.toJson()));
-        }
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/api/initialize", new MyHandler());
+        var server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/api/initialize", new MongoConfig.ContextHandler());
         server.setExecutor(null);
+        MongoConfig.Connect();
         server.start();
-    }
-
-    static class MyHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            var response = "inicializacao do projeto cloud";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
     }
 }
