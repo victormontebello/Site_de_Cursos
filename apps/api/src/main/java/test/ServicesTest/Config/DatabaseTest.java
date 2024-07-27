88d088c9bba5253b7a87f519411b586ca4d9091b
montebello <montebello@discente.com>
2024-07-27 17:33:48 -0300

    init testing with container database

---
+++ apps/api/src/main/java/test/ServicesTest/Config/DatabaseTest.java
@@ -0,0 +1,25 @@
+package test.ServicesTest.Config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class DatabaseTest {
+    private static MongoClient client;
+    private static MongoDatabase database;
+
+    public static void setupDatabase() {
+        String uri = System.getenv("MONGODB_ATLAS_CONN");;
+        client = MongoClients.create(uri);
+        database = client.getDatabase("test-db");
+    }
+
+    public static void teardownDatabase() {
+        database.drop();
+        client.close();
+    }
+
+    public static MongoDatabase getDatabase() {
+        return database;
+    }
+}
--- apps/api/src/main/java/test/ServicesTest/TesteServicoCurso.java
+++ apps/api/src/main/java/test/ServicesTest/TesteServicoCurso.java
@@ -1,20 +1,41 @@
 package test.ServicesTest;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.ufg.Infraestrutura.Servicos.ServicoUsuario;
 import org.ufg.Socket.Routes.ApiRoutes;
 import spark.Spark;
+import test.ServicesTest.Config.DatabaseTest;
 
 import static org.junit.Assert.assertEquals;
 
 public class TesteServicoCurso {
 
+    public ServicoUsuario servicoUsuario = new ServicoUsuario();
+    @Before
+    public void setUp() {
+        DatabaseTest.setupDatabase();
+        Spark.port(9999);
+        ApiRoutes.configurarRotas();
+        Spark.init();
+    }
+
+    @After
+    public void tearDown() {
+        Spark.stop();
+        DatabaseTest.teardownDatabase();
+    }
+
     @Test
-    public void test_obterTodos() {
+    public void test_obterTodos() throws Exception {
         Spark.port(9999);
         ApiRoutes.configurarRotas();
         Spark.init();
         var st = Spark.port();
         assertEquals(st, 9999);
+        var users = servicoUsuario.obterTodos();
+        assertEquals(users.size(), 0);
     }
 }
