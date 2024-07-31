6fc0cab41190111f5298bc328a4f8afc05281762
montebello <montebello@discente.com>
2024-07-31 16:38:34 -0300

    finalização de todos os testes unitários

--- apps/api/src/main/java/test/ServicesTest/TesteServicoCurso.java
+++ apps/api/src/main/java/test/ServicesTest/TesteServicoCurso.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.ufg.Domain.Enums.StatusEnum;
+import org.ufg.Domain.Exceptions.CursoNaoEncontradoException;
 import org.ufg.Domain.Models.Curso;
 import org.ufg.Infraestrutura.Servicos.ServicoUsuario;
 import org.ufg.Socket.Routes.ApiRoutes;
@@ -18,6 +19,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class TesteServicoCurso {
 
@@ -51,6 +53,45 @@ public class TesteServicoCurso {
         servicoCurso.Salvar(curso);
         var cursos = servicoCurso.obterTodos(null);
         assertEquals(cursos.size(), 1);
+    }
+
+    @Test
+    public void deve_obter_curso_por_id() throws Exception {
+        var curso = DadosParaTeste.ObterCursoParaTeste();
+        var cursoId = servicoCurso.Salvar(curso);
+        var cursoPorId = servicoCurso.obterPorId(cursoId.toHexString());
+        assertEquals(cursoPorId.get("nome").toString(), curso.getNome());
+    }
+
+    @Test
+    public void deve_atualizar_curso() throws Exception {
+        var curso = DadosParaTeste.ObterCursoParaTeste();
+        var cursoId = servicoCurso.Salvar(curso);
+        var cursoAtualizado = new Curso();
+        cursoAtualizado.setId(new ObjectId(cursoId.toHexString()));
+        cursoAtualizado.setNome("ATUALIZADO");
+        servicoCurso.Atualizar(cursoAtualizado);
+        var cursoAtualizadoPorId = servicoCurso.obterPorId(cursoId.toHexString());
+        assertEquals(cursoAtualizadoPorId.get("nome").toString(), cursoAtualizado.getNome());
+    }
+
+    @Test
+    public void deve_deletar_curso() throws Exception {
+        var curso = DadosParaTeste.ObterCursoParaTeste();
+        var cursoId = servicoCurso.Salvar(curso);
+        servicoCurso.Deletar(cursoId.toHexString());
+        var cursoDeletado = servicoCurso.obterPorId(cursoId.toHexString());
+        assertEquals(cursoDeletado.containsKey("_id"), false);
+    }
+
+    @Test
+    public void deve_retornar_excessao_de_curso_por_id() throws CursoNaoEncontradoException {
+        assertThrows(CursoNaoEncontradoException.class, () -> servicoCurso.obterPorId("123456789"));
+    }
+
+    @Test
+    public void deve_retornar_excessao_de_curso_nao_encontrado_ao_deletar() throws CursoNaoEncontradoException {
+        assertThrows(CursoNaoEncontradoException.class, () -> servicoCurso.Deletar("123456789"));
     }
 }
 
